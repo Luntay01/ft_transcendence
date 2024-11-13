c659a1d2510a972539ac3052f1ffccdc7f1e4897
hiono <hiono@student.42adel.org.au>
2024-11-13 18:43:56 +0900

    added User model

--- project/backend/users/models.py
+++ project/backend/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# Users
+# TODO: validate inputs
+# TODO: password hashed
+class User(models.Model):
+	username = models.CharField(max_length=256, primary_key=True)
+	password = models.CharField(max_length=256)
+	email = models.EmailField(max_length=256, default="test@example.com")
