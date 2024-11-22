b1c68bda3acae9d93d6f961bec37179a5cdd9bd3
hiono <hiono@student.42adel.org.au>
2024-11-22 11:56:16 +0900

    edit admin page for User model

--- project/backend/users/admin.py
+++ project/backend/users/admin.py
@@ -1,21 +1,41 @@
-from django.contrib import admin
-from .models import User
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django import forms
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import User
 
 # Register your models here.
-class UserForm(forms.ModelForm):
-	password = forms.CharField(
-		label=("Password"),
-		widget=forms.PasswordInput,
-	)
-
+class UserCreationForm(UserCreationForm):
 	class Meta:
 		model = User
-		fields = ['username', 'password', 'email']
+		fields = '__all__'
 
-class UserAdmin(admin.ModelAdmin):
-	list_display = ['username', 'password', 'email']
-	form = UserForm
+class UserChangeFrom(UserChangeForm):
+	class Meta:
+		model = User
+		fields = '__all__'
+
+class UserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+    list_display = ("email", "username", "is_staff", "is_active",)
+    list_filter = ("email", "username", "is_staff", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("email", "password", "username")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "username", "is_staff",
+                "is_active", "groups", "user_permissions"
+            )}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
 
 admin.site.register(User, UserAdmin)
