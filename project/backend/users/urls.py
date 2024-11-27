2a8fb781d967a130b5be14de1a3406b2b0cbecf5
hiono <hiono@student.42adel.org.au>
2024-11-27 16:59:53 +0900

    silently refresh and validate jwt in accessing to #home or #gamePong

--- project/backend/users/urls.py
+++ project/backend/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from .views import UserRegisterView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
