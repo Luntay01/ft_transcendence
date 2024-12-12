d0ad249c221fb407e96cf2bccf8c72b36e053ff0
kyle <kylemordaunt@gmail.com>
2024-12-12 21:34:17 +1030

    a collider manager currently only has one functiont to dectectCollisions

---
+++ project/frontend/src/js/games/gamePong/js/components/ColliderManager.js
@@ -0,0 +1,11 @@
+
+class ColliderManager
+{
+	static detectCollision(object1, object2)
+	{
+		const box1 = new THREE.Box3().setFromObject(object1);
+		const box2 = new THREE.Box3().setFromObject(object2);
+		return box1.intersectsBox(box2);
+	}
+}
+export default ColliderManager;
