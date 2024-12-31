c6e2eab79b0a8e810c228f19e6436e857b8cd4cc
hiono <hiono@student.42adel.org.au>
2024-12-31 16:32:15 +0900

    django authentication with oauth provider and oauth user id

--- project/backend/users/views.py
+++ project/backend/users/views.py
@@ -40,7 +40,38 @@ class OauthCodeView(views.APIView):
         headers = {'Authorization': f'Bearer {access_token}'}
         response = requests.get('https://api.intra.42.fr/v2/me', headers=headers)
         content = response.json()
-        logging.warning('id: ' + str(content["id"]))    #debug
-        logging.warning('username: ' + content["login"])    #debug
-        logging.warning('email: ' + content["email"])    #debug
-        return Response('', status.HTTP_200_OK)
+        # logging.warning('id: ' + str(content["id"]))    #debug
+        # logging.warning('username: ' + content["login"])    #debug
+        # logging.warning('email: ' + content["email"])    #debug
+
+        return Response(response.json(), status.HTTP_200_OK)
+    
+class TokenObtainPairView(jwt_views.TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        if not ('provider' in request.data.keys()):
+            return Response({'error': 'Auth provider is not provided'}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        provider = request.data['provider']
+
+        if (provider == 'Pong'):
+            user = User.objects.filter(email=request.data['email'], provider=provider)
+        elif (provider == '42Oauth'):
+            user = User.objects.filter(oauth_user_id=request.data['oauth_user_id'], provider=provider)
+        else:
+            return Response({'error': 'Auth provider is invalid'}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+        if (user.count() != 1):
+            return Response({'error': 'User does not exist'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        data = request.data
+        _mutable = data._mutable
+        data._mutable = True
+        data['id'] = user.get().id
+        data._mutable = _mutable
+        serializer = self.get_serializer(data=data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except jwt_exp.TokenError as e:
+            raise jwt_exp.InvalidToken(e.args[0])
+        
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
