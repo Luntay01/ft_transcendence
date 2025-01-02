260fa97b84f0d707000d583cb1ff66bba6b7523d
hiono <hiono@student.42adel.org.au>
2025-01-02 21:38:27 +0900

    set global variables for 42 Oauth Authorization App

--- project/backend/users/views.py
+++ project/backend/users/views.py
@@ -7,6 +7,7 @@ from .models import User
 from .serializers import UserSerializer, OauthCodeSerializer
 import requests
 import logging
+import os
 
 class UserRegisterView(views.APIView):
     permission_classes = [AllowAny]
@@ -19,23 +20,22 @@ class UserRegisterView(views.APIView):
 
 class OauthCodeView(views.APIView):
     permission_classes = [AllowAny]
-    #TODO: define global vars
     #TODO: remove logging (import as well)
     def post(self, request):
+        CLIENT_ID = os.environ.get('CLIENT_ID')
+        CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
         serializer = OauthCodeSerializer(data=request.data)
         if not (serializer.is_valid()):
             return Response(status.HTTP_422_UNPROCESSABLE_ENTITY)
         code = serializer.validated_data['code']
         logging.warning('code: ' + code)    #debug
-        client_id = "u-s4t2ud-7eb0d578913ab9934c2b116843901211c2e920a996f3a96f058464f1d33e1f38"
-        client_secret = "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"
         state = serializer.validated_data['state']
         logging.warning('state: ' + state)    #debug
         post_data = {
             'grant_type': 'authorization_code',
             'code': code,
-            'client_id': client_id,
-            'client_secret': client_secret,
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
             'redirect_uri': 'http://localhost:3000/callback',
             'state': state,
             }
