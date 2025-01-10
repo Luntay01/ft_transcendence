c522d4aaef261822519f885695d431a39748c657
hiono <hiono@student.42adel.org.au>
2025-01-10 18:15:52 +0900

    add first_name, last_name, birth_day in admin page

--- project/backend/users/admin.py
+++ project/backend/users/admin.py
@@ -20,17 +20,27 @@ class UserAdmin(UserAdmin):
     add_form = UserCreationForm
     form = UserChangeForm
     model = User
-    list_display = ("id", "email", "provider", "oauth_user_id", "username", "picture", "is_staff", "is_active",)
-    list_filter = ("id", "email", "provider", "oauth_user_id", "username", "picture", "is_staff", "is_active",)
+    list_display = (
+        "id", "email", "provider", "oauth_user_id", "username", "picture",
+        "first_name", "last_name", "birth_day", "is_staff", "is_active",
+        )
+    list_filter = (
+        "id", "email", "provider", "oauth_user_id", "username", "picture",
+        "first_name", "last_name", "birth_day", "is_staff", "is_active"
+        )
     fieldsets = (
-        (None, {"fields": ("email", "provider", "oauth_user_id", "password", "username", "picture")}),
+        (None, {"fields": (
+            "email", "provider", "oauth_user_id", "password", "username", "picture",
+            "first_name", "last_name", "birth_day"
+            )}),
         ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
     )
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
             "fields": (
-                "email", "provider", "oauth_user_id", "password1", "password2", "username", "is_staff",
+                "email", "provider", "oauth_user_id", "password1", "password2", "username",
+                "first_name", "last_name", "birth_date", "is_staff",
                 "is_active", "groups", "user_permissions"
             )}
         ),
