932db32b728ef4de1d7e57e9bdfe7aad2fa3f8d0
hiono <hiono@student.42adel.org.au>
2025-01-14 19:20:22 +0900

    fix user register API url

--- project/frontend/src/js/signup.js
+++ project/frontend/src/js/signup.js
@@ -8,7 +8,7 @@ export function setupSignupForm() {
         const provider = 'Pong';
 
         try {
-            const response = await fetch('http://localhost:8000/api/users/register/', {
+            const response = await fetch('http://localhost:8000/api/users/', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password, email, provider }),
